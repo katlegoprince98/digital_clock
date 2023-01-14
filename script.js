fb4a5300ce7206917b4e8978d706a572751e4f25
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-14 13:17:42 +0200

    js

---
+++ script.js
@@ -0,0 +1,4 @@
+const hour = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
