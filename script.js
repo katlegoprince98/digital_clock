44192222767893ad258b871c1a64c407bf9a2857
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-14 13:21:14 +0200

    25

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const hour = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const ampm = document.getElementById("ampm");
+
+function getTime(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+}
